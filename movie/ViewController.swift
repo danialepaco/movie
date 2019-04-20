7b0e2801d82e7b733e00467f50d5f196a52c9291
danialepaco <danialepaco@hotmail.com>
2019-04-20 17:33:42 -0400

    Initial Commit

---
+++ movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movie
+//
+//  Created by Daniel Parra on 4/20/19.
+//  Copyright © 2019 danialepaco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
